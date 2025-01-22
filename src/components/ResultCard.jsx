daffc444f46f097952c61ffdb21d2dcf9e79bd54
hws <haws91@gmail.com>
2025-01-22 21:42:05 +0900

    18

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -5,12 +5,12 @@ const ResultCard = ({ title, base, value, colors, offset = 0 }) => {
 
   return (
     <div
-      className={`rounded-lg p-6 shadow-md text-center`}
+      className={`rounded-lg p-6 shadow-md text-center flex flex-col`}
       style={{ backgroundColor: colors[Math.ceil(value / 10) - 1 + offset] }}
     >
       <h3 className="text-lg font-semibold mb-2">{title}</h3>
       <small>{base}</small>
-      <p className="text-4xl font-bold">
+      <p className="text-4xl font-bold mt-auto">
         {value !== null ? `${value}%` : "-"}
       </p>
     </div>
